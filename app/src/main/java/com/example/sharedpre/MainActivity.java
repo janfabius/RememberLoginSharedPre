ded3ff5291c32815b8aa9c4b62838c9455e0fac9
janfabius <adanet18@gmail.com>
2022-10-28 17:12:18 +0200

    upg

--- app/src/main/java/com/example/sharedpre/MainActivity.java
+++ app/src/main/java/com/example/sharedpre/MainActivity.java
@@ -67,4 +67,5 @@ public class MainActivity extends AppCompatActivity {
         edtPassword.setText(sharedPreferences.getString("password",""));
         cbSave.setChecked(sharedPreferences.getBoolean("save",false));
     }
+
 }
